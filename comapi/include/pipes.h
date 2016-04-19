7766c4b8e28625ebec035fe228678e12410cbdb3
Juan <jpmrno@gmail.com>
2016-04-19 14:57:11 -0300

    Added comapi makefile

--- comapi/include/pipes.h
+++ comapi/include/pipes.h
@@ -3,4 +3,9 @@
 
 #include <library.h>
 
+connection_t * pipe_make(address_t address);
+connection_t * pipe_connect(address_t address);
+int pipe_remove(connection_t * connection);
+int pipe_disconnect(connection_t * connection);
+
 #endif

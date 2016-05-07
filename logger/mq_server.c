d7d6484635ca52dcc2230cd5fb725b90bc9c08a7
natalia navas <natinavas@gmail.com>
2016-05-07 18:36:07 -0300

    fucniona message queues bien, hace falta separarlo y descabezearlo pero anda

--- logger/mq_server.c
+++ logger/mq_server.c
@@ -4,13 +4,13 @@
 #include <sys/types.h>
 #include <string.h>
 
-
 typedef struct msgbuf {
     long mtype;       /* message type, must be > 0 */
     char mtext[128];    /* message data */
 }msgbuf;
 
 int create_msq();
+int send_message(int mtype, char* msg);
 int recieve_msg();
 
 // key_t key; /* key to be passed to msgget() */
@@ -20,17 +20,27 @@ int recieve_msg();
 // 	/* msgflg argument must be an octal integer with settings for the queue's permissions and control flags.*/
 //
 
+static int msqid;
+static int msgflg = IPC_CREAT | 0666;
+
 int main(void){
 	create_msq();
-	recieve_msg();
 	
+	send_message(1, "holalalala");
+	send_message(1, "chauauauau");
+	send_message(1, "queueueueu");
+	send_message(1, "aprobamof3");
+	send_message(1, "eeeeeeeeee(como gaston)");
+
+	while(1){
+		recieve_msg();
+	}	
 }
 
 int create_msq(){
 
-	int msgflg = IPC_CREAT | 0666;
+	int msgflg;
 	key_t key = 1234;
-	int msqid; /* return value from msgget() */ 
 
 	if ((msqid = msgget(key, msgflg)) == -1)
   	{
@@ -39,12 +49,15 @@ int create_msq(){
    		return -1;
    	} else
 		printf("message queue identifier: %d\n", msqid);
-	
-	
+
+	return msqid;
+}
+
+
+int send_message(int mtype, char* msg){
 	msgbuf sbuf;
-	sbuf.mtype = 1;
-	strcpy(sbuf.mtext, "lDFKAJSPDFAJDSOIFOP");
-	
+	sbuf.mtype = mtype;
+	strcpy(sbuf.mtext, msg);
 	
 	if(msgsnd(msqid, &sbuf, 128, msgflg) < 0){
 		printf("se cago todo enviando\n");
@@ -53,13 +66,12 @@ int create_msq(){
 		printf("se envio el mensaje /o_o/\n");
 	}
 
-	return msqid;
+	return 0;
+	
 }
 
 int recieve_msg(){
-	int msqid;
 	key_t key = 1234;
-	
 	
 	printf("entre al recieve\n");
 	

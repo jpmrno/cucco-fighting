12c2f3d2556122b76d33033daf6f25028a1889e2
natalia navas <natinavas@gmail.com>
2016-05-12 21:33:56 -0300

    funciones de client middle falta la de cucco list y lo de lserver

--- client/include/clib.h
+++ client/include/clib.h
@@ -4,5 +4,12 @@
 #include <comapi.h>
 
 int login(connection_t connection, char * user);
+double money(connection_t connection);
+int cucco_add(connection_t connection, char * name);
+int cucco_remove(connection_t connection, char * name);
+char ** list(connection_t connection);
+int bet(connection_t connection, char * cucco, double money);
+int reset(connection_t connection);
+int exit(connection_t connection);
 
 #endif
--- client/src/clib.c
+++ client/src/clib.c
@@ -25,6 +25,28 @@
 // 	server_disconnect(connection);
 // }
 
+static void opcode(int op);
+
+static void opcode(opcode_t op) {
+	void * buffer;
+	int size;
+
+	if(tpl_jot(TPL_MEM, &buffer, &size, "i", &op) == -1) {
+		printf("2) ERROR\n");
+		return FALSE;
+	}
+
+	if(server_send(connection, &size, sizeof(int)) == -1) {
+		printf("3) ERROR\n");
+		return FALSE;
+	}
+
+	if(server_send(connection, buffer, size) == -1) {
+		printf("4) ERROR\n");
+		return FALSE;
+	}
+}
+
 int login(connection_t connection, char * user) {
 	tpl_node * node;
 	void * buffer;
@@ -35,6 +57,8 @@ int login(connection_t connection, char * user) {
 		return FALSE;
 	}
 
+	opcode(LOGIN);
+	
 	if(tpl_jot(TPL_MEM, &buffer, &size, "s", &user) == -1) {
 		printf("2) ERROR\n");
 		return FALSE;
@@ -75,3 +99,245 @@ int login(connection_t connection, char * user) {
 
 	return ret;
 }
+
+double money(connection_t connection) {
+	tpl_node * node;
+	void * buffer;
+	int size;
+	double ret;
+
+	if(connection == NULL) {
+		printf("1) ERROR\n");
+		return FALSE;
+	}
+
+	opcode(MONEY);
+
+	if(server_receive(connection, &size, sizeof(int)) == -1) {
+		printf("5) ERROR\n");
+		return FALSE;
+	}
+
+	buffer = malloc(size);
+	if(buffer == NULL) {
+		printf("6) ERROR\n");
+		return FALSE;
+	}
+
+	if(server_receive(connection, buffer, size) == -1) {
+		printf("7) ERROR\n");
+		return FALSE;
+	}
+
+	node = tpl_map("d", &ret);
+	tpl_load(node, TPL_MEM, buffer, size);
+	tpl_unpack(node, 0);
+	tpl_free(node);
+
+	printf("Return: %d\n", ret);
+
+	return ret;
+}
+
+int bet(connection_t connection, char * cucco, double money){
+	tpl_node * node;
+	void * buffer;
+	int size, ret;
+
+	if(connection == NULL) {
+		printf("1) ERROR\n");
+		return FALSE;
+	}
+	
+	opcode(BET);
+
+	if(tpl_jot(TPL_MEM, &buffer, &size, "sf", &cucco, &money) == -1) {
+		printf("2) ERROR\n");
+		return FALSE;
+	}
+
+	if(server_send(connection, &size, sizeof(int)) == -1) {
+		printf("3) ERROR\n");
+		return FALSE;
+	}
+
+	if(server_send(connection, buffer, size) == -1) {
+		printf("4) ERROR\n");
+		return FALSE;
+	}
+
+	if(server_receive(connection, &size, sizeof(int)) == -1) {
+		printf("5) ERROR\n");
+		return FALSE;
+	}
+
+	buffer = malloc(size);
+	if(buffer == NULL) {
+		printf("6) ERROR\n");
+		return FALSE;
+	}
+
+	if(server_receive(connection, buffer, size) == -1) {
+		printf("7) ERROR\n");
+		return FALSE;
+	}
+
+	node = tpl_map("i", &ret);
+	tpl_load(node, TPL_MEM, buffer, size);
+	tpl_unpack(node, 0);
+	tpl_free(node);
+
+	printf("Return: %d\n", ret);
+
+	return ret;
+	
+	
+}
+
+
+
+int cucco_add(connection_t connection, char * name){
+	tpl_node * node;
+	void * buffer;
+	int size, ret;
+
+	if(connection == NULL) {
+		printf("1) ERROR\n");
+		return FALSE;
+	}
+	
+	opcode(CUCCO_ADD);
+
+	if(tpl_jot(TPL_MEM, &buffer, &size, "s", &cucco) == -1) {
+		printf("2) ERROR\n");
+		return FALSE;
+	}
+
+	if(server_send(connection, &size, sizeof(int)) == -1) {
+		printf("3) ERROR\n");
+		return FALSE;
+	}
+
+	if(server_send(connection, buffer, size) == -1) {
+		printf("4) ERROR\n");
+		return FALSE;
+	}
+
+	if(server_receive(connection, &size, sizeof(int)) == -1) {
+		printf("5) ERROR\n");
+		return FALSE;
+	}
+
+	buffer = malloc(size);
+	if(buffer == NULL) {
+		printf("6) ERROR\n");
+		return FALSE;
+	}
+
+	if(server_receive(connection, buffer, size) == -1) {
+		printf("7) ERROR\n");
+		return FALSE;
+	}
+
+	node = tpl_map("i", &ret);
+	tpl_load(node, TPL_MEM, buffer, size);
+	tpl_unpack(node, 0);
+	tpl_free(node);
+
+	printf("Return: %d\n", ret);
+
+	return ret;
+}
+
+int cucco_remove(connection_t connection, char * name){
+	tpl_node * node;
+	void * buffer;
+	int size, ret;
+
+	if(connection == NULL) {
+		printf("1) ERROR\n");
+		return FALSE;
+	}
+	
+	opcode(CUCCO_REMOVE);
+
+	if(tpl_jot(TPL_MEM, &buffer, &size, "s", &cucco) == -1) {
+		printf("2) ERROR\n");
+		return FALSE;
+	}
+
+	if(server_send(connection, &size, sizeof(int)) == -1) {
+		printf("3) ERROR\n");
+		return FALSE;
+	}
+
+	if(server_send(connection, buffer, size) == -1) {
+		printf("4) ERROR\n");
+		return FALSE;
+	}
+
+	if(server_receive(connection, &size, sizeof(int)) == -1) {
+		printf("5) ERROR\n");
+		return FALSE;
+	}
+
+	buffer = malloc(size);
+	if(buffer == NULL) {
+		printf("6) ERROR\n");
+		return FALSE;
+	}
+
+	if(server_receive(connection, buffer, size) == -1) {
+		printf("7) ERROR\n");
+		return FALSE;
+	}
+
+	node = tpl_map("i", &ret);
+	tpl_load(node, TPL_MEM, buffer, size);
+	tpl_unpack(node, 0);
+	tpl_free(node);
+
+	printf("Return: %d\n", ret);
+
+	return ret;
+}
+
+int reset(connection_t connection){
+	tpl_node * node;
+	void * buffer;
+	int size;
+	double ret;
+
+	if(connection == NULL) {
+		printf("1) ERROR\n");
+		return FALSE;
+	}
+
+	opcode(RESET);
+
+	if(server_receive(connection, &size, sizeof(int)) == -1) {
+		printf("5) ERROR\n");
+		return FALSE;
+	}
+
+	buffer = malloc(size);
+	if(buffer == NULL) {
+		printf("6) ERROR\n");
+		return FALSE;
+	}
+
+	if(server_receive(connection, buffer, size) == -1) {
+		printf("7) ERROR\n");
+		return FALSE;
+	}
+
+	node = tpl_map("i", &ret);
+	tpl_load(node, TPL_MEM, buffer, size);
+	tpl_unpack(node, 0);
+	tpl_free(node);
+
+	printf("Return: %d\n", ret);
+
+	return ret;
+}
+

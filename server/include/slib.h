f5730ac0bc8fe9642f921479386fd4a3a6a5b322
fbartolome <fbartolome18@gmail.com>
2016-05-12 22:34:33 -0300

    reset & money

--- server/include/slib.h
+++ server/include/slib.h
@@ -4,5 +4,11 @@
 #include <comapi.h>
 
 int login(connection_t connection);
+int add_cucco(connection_t connection);
+int remove_cucco(connection_t* connection);
+int bet(connection_t* connection);
+int reset(connection_t* connection);
+int money(connection_t* connection);
+
 
 #endif

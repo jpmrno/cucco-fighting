c071cae7287e90ec0691be2e80e61d3f664847ec
natalia navas <natinavas@gmail.com>
2016-05-14 14:43:17 -0300

    unas wrappers de semaforos de internet utiles

--- tests/shared_memory/shared_mem.c
+++ tests/shared_memory/shared_mem.c
@@ -5,6 +5,15 @@
 
 #define SHMSZ 27
 
+#if !defined(__GNU_LIBRARY__) || defined(_SEM_SEMUN_UNDEFINED)
+union semun
+{
+    int val;                // value for SETVAL
+    struct semid_ds* buf;   // buffer for IPC_STAT, IPC_SET
+    unsigned short*  array; // array for GETALL, SETALL
+    struct seminfo*  __buf; // buffer for IPC_INFO
+}; #endif
+
 
 int start_shm();
 
@@ -61,4 +70,73 @@ int start_shm(){
    return 0;	
 	
 }
+
+/**
+ * Creates a new semaphore set.
+ *
+ * @param  n     Number of semaphores in set.
+ * @param  vals  Default values to start off with.
+ * @return Id of semaphore set.
+ */
+int CreateSemaphoreSet(int n, short* vals)
+{
+    union semun arg;
+    int id;
+    assert(n > 0);        /* You need at least one! */
+    assert(vals != NULL); /* And they need initialising! */
+    id = semget(IPC_PRIVATE, n, SHM_R | SHM_W);
+    arg.array = vals;
+    semctl(id, 0, SETALL, arg);
+	return id; 
+}
+
+
+
+/**
+ * Frees up the given semaphore set.
+ *
+ * @param  id  Id of the semaphore group.
+ */
+void DeleteSemaphoreSet(int id)
+{
+    if (semctl(id, 0, IPC_RMID, NULL) == -1)
+    {
+        perror("Error releasing semaphore!");
+        exit(EXIT_FAILURE);
+    }
+}
+/**
+ * Locks a semaphore within a semaphore set.
+ *
+ * @param  id  Semaphore set it belongs to.
+ * @param  i   Actual semaphore to lock.
+ *
+ * @note If it’s already locked, you’re put to sleep.
+ */
+void LockSemaphore(int id, int i)
+{
+    struct sembuf sb;
+    sb.sem_num = i;
+5 THE OBLIGATORY CRAPPY EXAMPLE: A CHAT SYSTEM 7
+sb.sem_op = -1;
+sb.sem_flg = SEM_UNDO;
+semop(id, &sb, 1);
+}
+/**
+ * Unlocks a semaphore within a semaphore set.
+ *
+ * @param  id  Semaphore set it belongs to.
+ * @param  i   Actual semaphore to unlock.
+ */
+void UnlockSemaphore(int id, int i)
+{
+    struct sembuf sb;
+    sb.sem_num = i;
+    sb.sem_op = 1;
+    sb.sem_flg = SEM_UNDO;
+    semop(id, &sb, 1);
+}
+
+
+
 
